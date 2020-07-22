709fcc73a8b706f21549982c474032a276d5ad57
Irfan Jauhari <kangrykong123@gmail.com>
2020-07-22 22:51:17 +0700

    fix cors

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+var http = require("http").Server(app);
 const tw = require("node-tweet-stream");
-const io = require("socket.io")(http);
-const cors = require("cors")
+const io = require("socket.io")(http, {
+  handlePreflightRequest: (req, res) => {
+    const headers = {
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+      "Access-Control-Allow-Credentials": true,
+    };
+    res.writeHead(200, headers);
+    res.end();
+  },
+});
+const cors = require("cors");
 require("dotenv").config();
 
-var http = require("http").createServer(app);
+app.use(cors());
+app.get("/", (req, res) => res.send("Hello World!"));
 
 t = new tw({
   consumer_key: process.env.TWEET_APIKEY,
@@ -24,13 +36,25 @@ t.track("onedirection");
 t.track("bucin");
 t.track("zonanyaman");
 t.track("kamukuat");
-t.on("tweet", function (tweet) {
-  io.emit("tweet", tweet);
+
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => {
+    t.on("tweet", function (tweet) {
+      socket.emit("tweet", tweet);
+    });
+  }, 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
 });
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.use(cors());
 http.listen(port, () => {
   console.log("listening on *:" + port);
 });
