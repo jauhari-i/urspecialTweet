d4f9215f1e127cb308633cfa42368172cf1235b5
Irfan Jauhari <kangrykong123@gmail.com>
2020-07-22 21:17:43 +0700

    initial

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = 4000 || process.env.PORT;
+const tw = require("node-tweet-stream");
+const io = require("socket.io")(http);
+require("dotenv").config();
+
+var http = require("http").createServer(app);
+
+t = new tw({
+  consumer_key: process.env.TWEET_APIKEY,
+  consumer_secret: process.env.TWEET_APITOKEN,
+  token: process.env.TWEET_CKEY,
+  token_secret: process.env.TWEET_CTOKEN,
+});
+
+t.on("error", function (err) {
+  console.log("Oh no");
+});
+
+t.track("quotes");
+t.track("onedirection");
+t.track("bucin");
+t.track("zonanyaman");
+t.track("kamukuat");
+t.on("tweet", function (tweet) {
+  io.emit("tweet", tweet);
+});
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+http.listen(port, () => {
+  console.log("listening on *:" + port);
+});
