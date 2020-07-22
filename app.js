0c986583dc255d7909ce43fa610a1ee76912eb36
Muhammad Irfan Jauhari <kangrykong123@gmail.com>
2020-07-22 21:50:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 4000 || process.env.PORT;
+const port = process.env.PORT || 4000;
 const tw = require("node-tweet-stream");
 const io = require("socket.io")(http);
 require("dotenv").config();
